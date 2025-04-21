c0b6d7cd11fad20a372912ab5a4ccfcaafbd5309
Anthony <anthony.rousseau26@gmail.com>
2025-04-21 19:32:21 +0200

    Initial commit

---
+++ src/main/java/fr/GlobalMarketSimApplication.java
@@ -0,0 +1,13 @@
+package fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalMarketSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalMarketSimApplication.class, args);
+    }
+
+}
